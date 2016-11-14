dc7713dd9d91ffbe588628e6c04fece6b9f2ad05
Nick Hoernle <nick@intellectionsoftware.com>
2016-11-14 12:05:57 -0500

    added a training and test module

--- data_exploration/load_yelp_data.py
+++ data_exploration/load_yelp_data.py
@@ -23,3 +23,22 @@ def load_yelp_dataframe(data_type):
         download(file_url, fullpath)
 
     return pandas.read_csv(fullpath)
+
+def create_standard_train_and_test(
+            dataframes,
+            train_size = 0.7,
+            seed=1000,
+        ):
+    '''
+    Accepts:
+     - dataframe=[businesses,users,reviews], array of input dataframes,
+     - train_size=0.7, the relative size of the training set
+     - seed=1000, the random seed to ensure we all have the same train/test sets
+    '''
+    test_train_tuples = []
+    for df in dataframes:
+        n,m = df.shape
+        train = df.sample(n=int(train_size*n), replace=False, random_state=seed)
+        test = df.drop(train.index)
+        test_train_tuples.append((train, test))
+    return test_train_tuples
